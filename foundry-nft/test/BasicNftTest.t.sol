6e95394ea73a6122a08dd9db89ad5b264b475537
ayoubioussoufa <ayoubioussoufa@gmail.com>
2024-04-24 12:54:28 +0100

    NFT SVG

--- foundry-nft/test/BasicNftTest.t.sol
+++ foundry-nft/test/BasicNftTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 
 import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {BasicNft} from "../src/BasicNft.sol";
-import {DeployBasicNft} from "../script/BasicNftDeploy.s.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
 contract TestBasicNft is Test {
     DeployBasicNft public deployBasicNft;
